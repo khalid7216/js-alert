9f5aa0dc5dfab5f8981a085bd0844429d324b0e2
khalid7216 <156210757+khalid7216@users.noreply.github.com>
2024-12-31 04:20:07 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let p = "!error enter a valid password";
+alert(p);
+
+let a = "Welcome to JS Land...\n\nHappy Coding!";
+alert(a);
+let b = "Welcome to JS Land.!"
+alert(b);
+let c = "Hello i can  run js through my web browser's console!";
+alert(c);
